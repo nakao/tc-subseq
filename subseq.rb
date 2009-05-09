c8f19415ce7231de23aeac8a80ecbb3c78bc9d59
中尾 光輝 <mitsuteru.nakao@gmail.com>
2009-05-09 12:35:29 +0900

    Added path argument for SubSeq.new.

--- subseq.rb
+++ subseq.rb
@@ -1,35 +1,50 @@
 require 'tokyocabinet'
 include TokyoCabinet
 
-  
+# Abstract class for querying sub-sequence to Tokyo Cabinet db.
+#
+# Inherited class should have folowing variables and method:
+# 1. @db for Tokyo Cabinet db instance.
+# 2. @dbname for file name of db
+# 3. #get_arg(i) for convert method from chunk to key
+#
 class SubSeq
+  WIDTH = 50    
+
+  # 
+  def self.subseq(chr, pos)
+    self.new(chr, pos).subseq
+  end
+
+  # 
   attr_reader :subseq
   
-  def initialize(chr, pos)
-    unless @db.open(@dbname)
+  # 
+  def initialize(pos, path = nil)
+    dbname = [path, @filename].compact.join("/")
+    unless @db.open(dbname)
       ecode = @db.ecode
-      STDERR.printf("open error: #{@dbname}: %s\n", db.errmsg(ecode))
+      STDERR.printf("open error: #{dbname}: %s\n", @db.errmsg(ecode))
       exit
     end
 
-    width = 50    
     start, stop = pos.split(",")
     start = start.to_i
     stop  = stop.to_i
 
-    chunk  = ((start - 1) / width) + 1
-    chunke = ((stop - 1) / width) + 1
+    chunk  = ((start - 1) / WIDTH) + 1
+    chunke = ((stop - 1) / WIDTH) + 1
 
     @subseq = ""
-    offset = start - (chunk - 1) * width
-    length = stop - (chunke - 1) * width
+    offset = start - (chunk - 1) * WIDTH
+    length = stop - (chunke - 1) * WIDTH
 
     (chunk..chunke).each do |i|
       value = @db.get(self.get_arg(i))
       if chunk == chunke
         value = value[offset-1, length]
       elsif chunk == i
-        value = value[offset-1, (width - offset + 1)]
+        value = value[offset-1, (WIDTH - offset + 1)]
       elsif chunke == i
         value = value[0, length]
       end
@@ -39,25 +54,30 @@ class SubSeq
   end
 end
 
-
+# Implimantation of sub-sequence extraction using TCF Tokyo Cabinet Fixed-length Index.
 class TCF_SS < SubSeq
   def initialize(chr, pos)
-    @dbname = "#{chr}.tcf"
+    @filename = "#{chr}.tcf"
     @db = FDB::new
-    super(chr, pos)
+    super(pos)
   end
+
+  protected
   
   def get_arg(i)
     "#{i}"
   end
 end
 
+
 class TCH_SS < SubSeq
   def initialize(chr, pos)
-    @dbname = "#{chr}.tch"
+    @filename = "#{chr}.tch"
     @db = HDB::new
-    super(chr, pos)
+    super(pos)
   end
+
+  protected
   
   def get_arg(i)
     "#{chr}_#{i}"
