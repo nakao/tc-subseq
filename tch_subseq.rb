72421a7ae6c1eaee9dfb90aaa61d8bdf0670c47a
中尾 光輝 <mitsuteru.nakao@gmail.com>
2009-05-09 12:07:40 +0900

    Updated.

--- tch_subseq.rb
+++ tch_subseq.rb
@@ -1,12 +1,10 @@
 require 'subseq'
 
-if __FILE__ == $0
-  if ARGV.size < 1
-    STDERR.puts "Usage: ruby tch_subseq.rb chr1:100,110"
-    exit
-  end
-
-  arg = ARGV.shift
-  chr, pos = arg.split(":")
-  puts TCH_SS.new(chr, pos).subseq
+if ARGV.size < 1
+  STDERR.puts "Usage: ruby tch_subseq.rb chr1:100,110"
+  exit
 end
+
+arg = ARGV.shift
+chr, pos = arg.split(":")
+puts TCH_SS.new(chr, pos).subseq
